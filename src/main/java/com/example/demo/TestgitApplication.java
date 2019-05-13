f26528ec620841fc2c75bf9316575441c38ef70c
githublms <lms_ajdxwz@163.com>
2019-05-13 09:07:04 +0800

    本地库的第一次提交，用于测试冲突，作者是githublmsdemo

--- src/main/java/com/example/demo/TestgitApplication.java
+++ src/main/java/com/example/demo/TestgitApplication.java
@@ -9,7 +9,7 @@ public class TestgitApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestgitApplication.class, args);
 		System.err.println("this is my first add data");
-		System.err.println("this is my second add data");
+		System.err.println("this is my second add data，edit by githublmsdemo ,iam testing");
 	}
 
 }
