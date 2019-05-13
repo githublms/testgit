5f8ef2dae93601ef2fca20c30b9b07fdf60e1c59
githublms <lms_ajdxwz@163.com>
2019-05-13 09:02:16 +0800

    本地库的第三次提交，用于测试解决冲突的情况

--- src/main/java/com/example/demo/TestgitApplication.java
+++ src/main/java/com/example/demo/TestgitApplication.java
@@ -9,7 +9,7 @@ public class TestgitApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestgitApplication.class, args);
 		System.err.println("this is my first add data");
-		System.err.println("this is my second add data");
+		System.err.println("this is my second add data，edit by githublms ，i am is testing");
 	}
 
 }
